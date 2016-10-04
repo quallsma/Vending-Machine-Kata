022e8d918e63ea8ad11182c62b1b77ff83fb7895
quallsma <quallsmg59@gmail.com>
2016-10-04 18:20:28 -0400

    Initial Commit
    Initial Commit

---
+++ VendingMachineKata/VendingMachineKata/VendingMachineTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineKata
+{
+    class VendingMachineTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
